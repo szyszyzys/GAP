452696403c95f4a959a53008f35896d55e7cae3e
Zeyu Song <szyszy0315@gmail.com>
2023-09-14 00:39:49 -0400

    update_train

--- core/trainer/trainer.py
+++ core/trainer/trainer.py
@@ -83,7 +83,7 @@ class Trainer:
 
         if checkpoint:
             os.makedirs('checkpoints', exist_ok=True)
-            checkpoint_path = os.path.join('checkpoints', f'save.pt')
+            checkpoint_path = os.path.join('checkpoints', f'{prefix}save.pt')
             torch.save(self.model.state_dict(), checkpoint_path)
 
         if val_dataloader is None:
